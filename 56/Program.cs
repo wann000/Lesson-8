7e78608b2ab87e7e76da03835cb4b5b6c6671021
wann000 <wann0@yandex.ru>
2022-12-02 00:22:14 +0300

    56

---
+++ 56/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
